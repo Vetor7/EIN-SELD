1fead2d132cca227f31f98aa100f22f46f3c0a69
zyb <xmuzhangyuebin@gmail.com>
2024-08-24 16:46:35 +0800

    move configs

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-CONFIG_FILE='./modules/ein_seld/seld.yaml'
+CONFIG_FILE='./configs/seld.yaml'
 
 python modules/main.py -c $CONFIG_FILE train --seed=$(shuf -i 0-10000 -n 1) --num_workers=8
